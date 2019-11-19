90aeda1f07fcfe4c9caa5aeaffbdb7e2f8983877
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-19 10:30:29 +0100

    Hallo Welt

--- HalloWPF/HalloWPF/MainWindow.xaml.cs
+++ HalloWPF/HalloWPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace HalloWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hallo Welt");
+        }
     }
 }
