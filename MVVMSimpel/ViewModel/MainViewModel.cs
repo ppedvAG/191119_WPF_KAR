1a7fbe482001dcd751247f266d4d76c405fafa07
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-21 16:03:56 +0100

    INotifyPropertyChanged und Listentemplates

--- MVVMSimpel/ViewModel/MainViewModel.cs
+++ MVVMSimpel/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Model;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,7 @@ using System.Windows.Input;
 
 namespace ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
         public MainViewModel()
         {
@@ -18,9 +19,12 @@ namespace ViewModel
         private void GetPeople()
         {
             Personenliste = service.GetAllPeople();
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Personenliste")); // Sagt dem UI "Hey, Personenliste hat sich verändert -> Aktualisiere das Binding !!!!"
         }
 
         private PersonenService service;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public List<Person> Personenliste { get; set; }
         public ICommand GetPeopleCommand { get; set; }
