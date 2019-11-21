64c62c4c40130b7f9e21e57c3bb051a36260cf5b
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-21 15:31:35 +0100

    Model mit Geschäftslogik erstellt

---
+++ MVVMSimpel/Model/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Person
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public byte Alter { get; set; }
+        public decimal Kontostand { get; set; }
+    }
+}
---
+++ MVVMSimpel/Model/PersonenService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Model
+{
+    public class PersonenService
+    {
+        public List<Person> GetAllPeople()
+        {
+            // Todo: Daten aus einer DB laden, REST-Service ansprechen etc...
+            return new List<Person>
+            {
+                new Person{Vorname="Tom",Nachname="Ate",Alter=10,Kontostand=100},
+                new Person{Vorname="Anna",Nachname="Nass",Alter=20,Kontostand=200},
+                new Person{Vorname="Peter",Nachname="Silie",Alter=30,Kontostand=300},
+            };
+        }
+    }
+}
