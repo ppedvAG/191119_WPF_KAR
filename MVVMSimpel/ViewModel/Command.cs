30d08fd3dd807ee7f901bb35c0ebb47d842d80db
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-21 15:44:02 +0100

    Einfaches ViewModel erstellt

---
+++ MVVMSimpel/ViewModel/Command.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class Command : ICommand
+    {
+        public Command(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        private Action execute;
+
+        public event EventHandler CanExecuteChanged;
+        public bool CanExecute(object parameter)
+        {
+            return true; // Kommando darf IMMER ausgeführt werden ;)
+        }
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
---
+++ MVVMSimpel/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            service = new PersonenService();
+            GetPeopleCommand = new Command(GetPeople);
+        }
+
+        private void GetPeople()
+        {
+            Personenliste = service.GetAllPeople();
+        }
+
+        private PersonenService service;
+
+        public List<Person> Personenliste { get; set; }
+        public ICommand GetPeopleCommand { get; set; }
+    }
+}
