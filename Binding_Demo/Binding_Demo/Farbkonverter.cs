e70b22061a82d362812a8cd2c89f625663566a2c
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-21 10:07:38 +0100

    MultiBinding

--- Binding_Demo/Binding_Demo/Farbkonverter.cs
+++ Binding_Demo/Binding_Demo/Farbkonverter.cs
@@ -32,4 +32,19 @@ namespace Binding_Demo
             throw new NotImplementedException();
         }
     }
+
+
+    class TextConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            // return $"{values[0]} {values[1]}";
+            return values[0].ToString() + " " + values[1].ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
