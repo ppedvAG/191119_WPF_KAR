5ca56bb61e54179ee890d04648c2cc15a52e1adb
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-21 09:59:53 +0100

    Einfaches Binding mit Konverter

---
+++ Binding_Demo/Binding_Demo/Farbkonverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Binding_Demo
+{
+    class Farbkonverter : IValueConverter
+    {
+        // One Way
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string text = (string)value;
+
+            if (text == "Rot")
+                return Brushes.Red;
+            else if (text == "Blau")
+                return Brushes.Blue;
+            else if (text == "Grün")
+                return Brushes.Green;
+            else
+                return Brushes.Black;
+        }
+
+        // TwoWay
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
