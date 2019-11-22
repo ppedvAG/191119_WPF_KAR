743ae22083aba86fbffea2cb607ae14d0b323061
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-22 11:45:23 +0100

    EmailTextBox mit ValidationRule und ErrorTemplate

---
+++ Validierung/Validierung/EmailTextBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Validierung
+{
+    /// <summary>
+    /// Interaction logic for EmailTextBox.xaml
+    /// </summary>
+    public partial class EmailTextBox : UserControl
+    {
+        public EmailTextBox()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+
+
+        public string Email
+        {
+            get { return (string)GetValue(EmailProperty); }
+            set { SetValue(EmailProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Email.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EmailProperty =
+            DependencyProperty.Register("Email", typeof(string), typeof(EmailTextBox), new PropertyMetadata(""));
+
+
+    }
+}
---
+++ Validierung/Validierung/EmailValidationRule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Validierung
+{
+    class EmailValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (string.IsNullOrWhiteSpace(value.ToString()))
+                return new ValidationResult(false, "Das Textfeld ist leer");
+
+            // Regex
+            //    regexr.com -> Tool
+            // oder : http://emailregex.com/
+
+            string email = (string)value;
+
+            if (Regex.Match(email, @"[a-zA-Z0-9.äöüÄÖÜß]+@[a-zA-Z0-9.äöüÄÖÜß]+\.[a-zA-Z]{2,3}").Success)
+            {
+                return new ValidationResult(true, "");
+            }
+            else
+                return new ValidationResult(false, "Ungültige Emailadresse");
+
+        }
+    }
+}
