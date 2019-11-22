0fa9d3b991c25cc956097225e4c8e73354c79536
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-22 15:50:37 +0100

    Kleine CSV-Datei einlesen und in der Liste darstellen

---
+++ CSVEinlesen/CSVEinlesen/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSVEinlesen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // File: Alles auf einmal:
+
+            string[] allLines = File.ReadAllLines("demo.csv");
+            foreach (var line in allLines)
+            {
+                Person p = new Person();
+                if (string.IsNullOrWhiteSpace(line))
+                    continue; // Leerzeile ignorieren
+
+                string[] values = line.Split(';');
+
+                p.Vorname = values[0];
+                p.Nachname = values[1];
+                p.Alter =  Convert.ToByte(values[2]);
+                p.Kontostand = Convert.ToDecimal(values[3]);
+
+                listBoxPersonen.Items.Add(p);
+            }
+        }
+    }
+}
