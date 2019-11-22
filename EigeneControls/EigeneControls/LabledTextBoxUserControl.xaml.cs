c3a2ff9e8971e8d1e101c8a9fec21f4dcf51ebf0
Michael Zöhling <michael.zoehling@outlook.com>
2019-11-22 09:49:14 +0100

    DependencyProperty mit propdp erstellt

--- EigeneControls/EigeneControls/LabledTextBoxUserControl.xaml.cs
+++ EigeneControls/EigeneControls/LabledTextBoxUserControl.xaml.cs
@@ -26,8 +26,45 @@ namespace EigeneControls
             this.DataContext = this; // Ich bin meine eigene Standarddatenquelle für Bindings
         }
 
+        // Normales .NET Property:
+        //public string LabelText { get; set; } // Binding mit der Kurzschreibweise
+        //public string TextBoxText { get; set; }
 
-        public string LabelText { get; set; } // Binding mit der Kurzschreibweise
-        public string TextBoxText { get; set; }
+
+        // 1)
+        // propdp + TAB + TAB 
+        // 2) 
+        // mit TAB durch alle 4 möglichkeiten Durchklicken:
+        // * Datentyp (int/string/.... wählen)
+        // * Name (name von eurem Property)
+        // * OwnerClass -> Klassenname von eurem UserControl
+        // * PropertyMetaData: Standardwert, wenn das ding leer ist
+
+
+
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LabelText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelTextProperty =
+            DependencyProperty.Register("LabelText", typeof(string), typeof(LabledTextBoxUserControl), new PropertyMetadata(""));
+
+
+        public string TextBoxText
+        {
+            get { return (string)GetValue(TextBoxTextProperty); }
+            set { SetValue(TextBoxTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TextBoxText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextBoxTextProperty =
+            DependencyProperty.Register("TextBoxText", typeof(string), typeof(LabledTextBoxUserControl), new PropertyMetadata(""));
+
+
+
     }
 }
